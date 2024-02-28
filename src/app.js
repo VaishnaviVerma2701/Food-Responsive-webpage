2eefcf2378fd4ef3c2ba48b20b3d639905bbfb2c
vishalguptamonu <vishalkrmonu@gmail.com>
2024-02-28 14:33:22 +0530

    code

---
+++ src/app.js
@@ -0,0 +1,80 @@
+
+
+// app.js starting
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/newfolder")
+  .then(() => {
+    console.log(`Connection successful`);
+  })
+  .catch((error) => {
+    console.error(`Error connecting to the database: ${error.message}`);
+  });
+// Your server code here (assuming it's running on port 3000)
+const express = require("express");
+const path =require("path");
+const app = express();
+const port = 4000;
+// require("./db/conn.js");
+
+const static_path =path.join(__dirname, "../");
+app.use(express.static(static_path));
+
+// Define the schema
+const fdSchema = new mongoose.Schema({
+  name: String,
+  email:String,
+  phone: Number,
+  message: String,
+});
+
+// Create a model
+const FD = mongoose.model("FD", fdSchema);
+// Body parser middleware
+app.use(express.urlencoded({ extended: true }));
+// Handle form submissions
+app.post("/", async (req, res) => {
+  try {
+    // Create a new GM instance
+    const fdMember = new FD({
+      name: req.body.name,
+      email:req.body.email,
+      phone: req.body.phone,
+      message: req.body.message,
+    });
+
+    // Save the data to the database
+    await fdMember.save();
+    res.send("Data saved successfully!");
+  } catch (error) {
+    console.error("Error saving data to the database:", error.message);
+    res.status(500).send("Error saving data to the database");
+  }
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from the chegg");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
+
+
+
+// test> use newfolder
+// switched to db newfolder
+// newfolder> show collections
+// gms
+// users
+// newfolder> db.gms.find()
+// [
+//   {
+//     _id: ObjectId("654f5c10905a9f70b2fb7bc7"),
+//     name: 'Vishal Kumar',
+//     age: 33,
+//     gender: 'male',
+//     locality: 'indian',
+//     __v: 0
+//   }
+// ]
